ac7cfb9aeadfedcc1e56d1b5c12379ba257976d4
Matthew Hayden <matthayden09@gmail.com>
2020-12-09 18:37:05 -0600

    made local storage save function for 9am

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDay = moment().format('LL');
+
+$("#currentDay").text(currentDay)
+
+// variables
+// when I click button
+$('#btn').on('click', function(event){
+    var text = $('#text').val();
+    event.preventDefault()
+    localStorage.setItem('9AM Input', text);
+    
+
+    console.log(event)
+})
+// inner text is selected
+// saved text goes to local storage
